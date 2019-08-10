0a6668795a5e44d396bd15efe5da9af1ce9258eb
kodetop <info@kodetop.com>
2019-08-09 21:20:43 -0500

    Estructura inicial del componente

---
+++ upload.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kodetop;
+
+class Upload
+{
+    // declare parameters
+
+    public function __construct()
+    {
+
+    }
+
+    public function allowMimeTypes()
+    {
+
+    }
+
+    public function setMaxFileSize()
+    {
+
+    }
+
+    public function setRemoteFolder()
+    {
+
+    }
+
+    public function save()
+    {
+
+    }
+}
