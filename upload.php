6cbfeb36b6066aead6cbafeeb864624aab2c3ad4
kodetop <info@kodetop.com>
2019-08-09 22:15:00 -0500

    Versión inicial de la clase

--- upload.php
+++ upload.php
@@ -4,30 +4,49 @@ namespace Kodetop;
 
 class Upload
 {
-    // declare parameters
+    const VERSION = '1.0';
+
+    private $maxFileSize = false;
+    private $mimesAllowed = [];
+    private $folder;
 
     public function __construct()
     {
 
     }
 
-    public function allowMimeTypes()
+    public function allowMimeTypes(array $mimes)
     {
-
+        $this->mimesAllowed = $mimes;
     }
 
-    public function setMaxFileSize()
+    public function setMaxFileSize(int $size)
     {
-
+        $this->maxFileSize = $size;
     }
 
-    public function setRemoteFolder()
+    public function setRemoteFolder(string $folder)
     {
-
+        $this->folder = $folder;
     }
 
-    public function save()
+    public function save(string $field, array $options = [])
     {
+        $file = $_FILES[$field]['tmp_name'];
+        $name = $_FILES[$field]['name'];
+        $size = $_FILES[$field]['size'];
+        $mime = mime_content_type($file);
+        $error = $_FILES[$field]['error'];
 
+        $folder = ($options['folder'] != '') ? $options['folder'] : $this->folder;
+
+        if ($error === UPLOAD_ERR_OK) {
+            // upload OK
+            if (move_uploaded_file($file, $folder . "/" . $name)) {
+                return (object) ['status' => true, 'name' => $name];
+            } else {
+                return (object) ['status' => false, 'error' => 'File was not uploaded please try again'];
+            }
+        }
     }
 }
